13faa1aaf35d5586f388fed2f04a46d4cf864f04
Patrick Kasiala <dev@kasiala.ch>
2020-04-20 18:14:59 +0200

    get user input

--- src/controllers/GetInputData.js
+++ src/controllers/GetInputData.js
@@ -2,12 +2,40 @@
 
 const moment = require("moment")
 const momentTZ = require('moment-timezone')
+const EventEmitter = require("eventemitter3")
+/*
+ * @param {HTMLInputElement} input_tz
+ * @param {HTMLInputElement} input_standby
+ * @param {HTMLInputElement} input_standby_start
+ * @param {HTMLInputElement} input_standby_end
+ * @param {HTMLInputElement} input_fdp_start_dat
+ * @param {HTMLInputElement} input_fdp_start_tim
+ * @param {HTMLInputElement} input_legs
+ * @param {HTMLInputElement} input_dep
+ * @param {HTMLInputElement} input_arr
+ * @param {HTMLInputElement} input_split
+ * @param {HTMLInputElement} input_split_start
+ * @param {HTMLInputElement} input_split_end
+*/
 
+/**
+ * @param {JSON} input_Elements
+ */
+function GetInputData(input_elements){
+        this.input_elements = input_elements
+}
+
+GetInputData.prototype.startToLocal = function(){
+    console.log(this.input_elements)
+}
+/*
 let a = moment()
 console.log(a.format(),"//moment().format()")
 console.log(a.tz("America/Toronto").format())
 console.log(a.utc().format())
 console.log(moment.tz.names())
+*/
+
 
 
 module.exports = GetInputData
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 "use strict"
 require("../scss/index.scss")
 
-const GetInputDate = require("./controllers/GetInputData")
+const GetInputData = require("./controllers/GetInputData")
+
+const getInputData = new GetInputData(
+    {
+        "regulation" : document.getElementById("regulationInput").value,
+        "tz" : document.getElementById("timezoneInput").value,
+        "stdb" : document.getElementById("standbyInput").checked,
+        "fdpStartDate" : document.getElementById("fdpstartDateInput").value,
+        "fdpStartTime": document.getElementById("fdpstartTimeInput").value,
+        "legs" : document.getElementById("legsInput").value,
+        "dep" : document.getElementById("departureLocationInput").value,
+        "arr" : document.getElementById("arrivalLocationInput").value,
+        "split" : document.getElementById("splitdutyInput").checked,
+        "splitStart" : document.getElementById("splitdutystartInput").value,
+        "splitEnd" : document.getElementById("splitdutyendInput").value,
+    }
+)
+
+getInputData.startToLocal()
