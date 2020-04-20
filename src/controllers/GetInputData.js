708f77260bc78c0df473d7c923433977f88f8a0b
Patrick Kasiala <dev@kasiala.ch>
2020-04-20 14:41:46 +0200

    test moment

--- src/controllers/GetInputData.js
+++ src/controllers/GetInputData.js
@@ -1,8 +1,13 @@
 "use strict"
 
-const moment = require('moment')
+const moment = require("moment")
 const momentTZ = require('moment-timezone')
 
-console.log(moment.format())
+let a = moment()
+console.log(a.format(),"//moment().format()")
+console.log(a.tz("America/Toronto").format())
+console.log(a.utc().format())
+console.log(moment.tz.names())
+
 
 module.exports = GetInputData
