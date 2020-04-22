c34089053fcd8a2e04bd38a5c0efac7ce2f89d7a
Patrick Kasiala <dev@kasiala.ch>
2020-04-22 13:17:23 +0200

    airport filter

--- src/controllers/MenuControl.js
+++ src/controllers/MenuControl.js
@@ -21,6 +21,30 @@ function MenuControl(input_elements){
 MenuControl.prototype.init=function(){
     document.getElementById("standbystarttimeInputGroup").style.display='none'
     document.getElementById("splitInputGroup").style.display='none'
+    //document.getElementById("arrivalLocationInput").addEventListener()
+    on(".input-arrAirport", "keyup", (event) => {
+        const term = event.handleObj.value
+        this.apLookup(term)
+      })
+    
+
+}
+
+MenuControl.prototype.apLookup=function(term){
+    console.log(term)
+    /*let matches = aiportsTz.filter((airport)=>{
+        const regex = new RegExp(`^${term}`,`gi`)
+        return airport.iata.match(regex)
+    })*/
+    let matches = aiportsTz.filter((airport)=>{
+        const regex = new RegExp(`^${term}`,`gi`)
+        if (airport.iata){
+            //console.log(airport.iata)
+            return airport.iata.match(regex) || airport.icao.match(regex)
+
+        }
+    })
+    console.log("matches",matches)
 
 }
 
